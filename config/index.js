3aedeaed04ae7f4d887830a21bbfc3f7605a089d
Luiz Felipe Prado Borba <95880863+Luiz-Borba@users.noreply.github.com>
2023-07-08 12:16:50 -0300

    Frist commit

---
+++ config/index.js
@@ -0,0 +1,57 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
+// Botão mobile
+let btnMobile = document.querySelector("#btn-mobile");
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+function toggleMenu(event) {
+
+    if (event.type === 'touchstart') {
+        event.preventDefault();
+    }
+
+    const nav = document.querySelector("#nav");   
+
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');   
+    }
+}
+// Botão mobile
